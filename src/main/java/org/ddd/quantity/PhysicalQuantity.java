e1a8cf0cfebca912e020b5abaaf8303a3e12add3
Michael1024-CJX <1024888599@qq.com>
2021-11-02 10:54:24 +0800

    docs:增加物理量的描述

--- src/main/java/org/ddd/quantity/PhysicalQuantity.java
+++ src/main/java/org/ddd/quantity/PhysicalQuantity.java
@@ -7,7 +7,15 @@ import org.ddd.unit.UnitWithPower;
 import org.ddd.util.NumberUtil;
 
 /**
- * 物理量，由数值和单位组成
+ * 物理量，由数值和单位组成。同时，物理量具有一些行为：
+ * 1. 转换。物理量应该能够进行单位转换，如 1m = 100cm, 1kg = 1000g。
+ * 转换在 “运算” 和 “比较” 中还具有重要的用途。
+ * 数量转换依赖于单位转换 {@link MeasurementUnit}
+ * 2. 算术运算。能够将两个物理量进行加减乘除的运算，运算不仅要考虑值，还需要考虑单位。
+ * 如：100cm + 1m = 200cm， 100cm * 1m = 10000cm^2。
+ * 3. 比较。两个同量度 {@link org.ddd.unit.Measurement} 的数量应该具有比较的功能，数量的单位不同的情况下，需要转换成相同的单位。
+ * 如：180cm > 175cm, 6kg > 5000g等。
+ *
  *
  * @author chenjx
  */
